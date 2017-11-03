dbd9469c461a7b06624bbb658108c9edf3c5b463
0101Q <32064797+0101Q@users.noreply.github.com>
2017-11-03 21:05:58 +0800

    CGUIMOOP_HW3
    11/03

---
+++ cguimjava2-hw3-0101Q/HW3.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+//import java.util.Scanner;
+
+/**
+ * @author TODO: B0544145 曹潔妮
+ * Try to write some comments for your codes (methods, 10 points)
+ */
+public class HW3 {
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("input N(deck of cards):");
+		//String testn= sc.nextLine(); 
+		int nDeck=1;
+		//TODO: please add new fields and methods to Deck class (35)做了
+		//usedCard (5 points)ok
+		//nUsed (5 points)ok
+		//getOneCard (10 points)ok
+		//shuffle (10 points)ok
+		//constructor (5 points)ok
+		Deck deck=new Deck(nDeck);
+		
+		//TODO: please check your output, make sure that you print newCard and newCard2 on your screen  (10 points)ok
+		//TODO: please add new fields and methods to Card class (25)做了
+		//Use enumerated type in Card (10 points)ok
+		//Constructor (5 points)ok
+		//printCard (5 points)ok
+		//getSuit (5 points)ok
+		Card newCard=deck.getOneCard();
+		newCard.printCard();
+		Card newCard2=deck.getOneCard();
+		newCard2.printCard();
+		deck.shuffle();
+		if(isAllCardsCorrect(deck.getAllCards(),nDeck)){
+			if(!isShuffleWorking(deck,newCard,newCard2)){
+				System.out.println("All Card: Well done! But shufller is not working");
+			}else{
+				System.out.println("Well done!");
+			}
+			
+		}else{
+			System.out.println("All Card: Error, please check your sourse code");
+		}
+
+	}
+	/**
+	 * This method is used for checking your result, not a part of your HW3
+	 */
+	private static boolean isShuffleWorking(Deck deck,Card newCard,Card newCard2){
+		deck.shuffle();
+		boolean isCorrect=true;
+		if(newCard.getSuit().equals(newCard2.getSuit()) &&
+				newCard.getRank()==newCard2.getRank()){
+					isCorrect=false;
+					return isCorrect;
+		}
+		for(int i=0;i<53;i++){
+			deck.getOneCard();
+		}
+		if(deck.nUsed!=1){
+			isCorrect=false;
+		}
+		return isCorrect;
+	}
+	
+	private static boolean isAllCardsCorrect(ArrayList<Card> allCards,int nDeck){
+		//check the output 
+		boolean isCorrect=true;;
+		HashMap <String,Integer> checkHash=new HashMap<String,Integer>();
+		for(Card card:allCards){
+			Card.Suit suit= card.getSuit();
+			int rank = card.getRank();
+			if(rank>13||rank<1){
+				isCorrect=false;
+				break;
+			}
+			if(checkHash.containsKey(suit+","+rank)){
+				checkHash.put(suit+","+rank, 
+						checkHash.get(suit+","+rank)+1);
+			}else{
+				checkHash.put(suit+","+rank, 1);
+			}
+
+		}
+		if(checkHash.keySet().size()==52){
+			for(int value:checkHash.values()){
+				if(value!=nDeck){
+					isCorrect=false;
+					break;
+				}
+			}
+		}else{
+			isCorrect=false;
+		}
+		return isCorrect;
+	}
+}
