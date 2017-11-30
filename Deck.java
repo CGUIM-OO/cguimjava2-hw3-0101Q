d5bbcc4d7f3f31ee942b99dc1a2844c2d1918889
0101Q <32064797+0101Q@users.noreply.github.com>
2017-12-01 00:03:29 +0800

    NEW
    NEW

--- Deck.java
+++ Deck.java
@@ -4,6 +4,10 @@ import java.util.Random;
 public class Deck {
 	private ArrayList<Card> cards;//放會使用的牌
 	private ArrayList<Card> usedCard;//放被使用過的牌
+	//增(1)
+	private ArrayList<Card> openCard; //（5pt）
+	//存放此副牌中所有打開的牌，洗牌時要重置
+	//END(1)
 	public int nUsed=0;//表使用過的牌的數量，並給初始值為零;
 	//TODO: Please implement the constructor (30 points)做了
 	public Deck(int nDeck){//建立以"副為單的牌"
@@ -33,33 +37,52 @@ public class Deck {
 	}	
 	
 	public void shuffle() {//洗牌，意指全部牌收回（不管發出去幾張），重新洗牌
-		usedCard=new ArrayList<Card>();//實體化
+		//增(2)
+		//洗牌時記得重置private ArrayList openCard;（5pt）
+		openCard=new ArrayList<Card>();	//實體化
+		openCard.clear();//將其清空
+		//END(2)
+		//usedCard=new ArrayList<Card>();//實體化
 		nUsed=0;
 		int j=0;//用來存隨機的數字
 		Random rnd = new Random();//用摨取隨機數字
 		//此時usedCard會是空的，用來存被隨機取出的牌
-			while(!cards.isEmpty())//洗完牌後cards將會變空的，所以當還沒洗完牌時將會持續while
+			//while(!cards.isEmpty())//洗完牌後cards將會變空的，所以當還沒洗完牌時將會持續while
+			while(nUsed!=cards.size()-1)//當所有牌都跑過一次才停止，減一是因為nUsed從零開始算，這時nUsed表洗過幾張牌
 			{ 
-				j =rnd.nextInt(cards.size());//cards裡的牌將被逐一取出，其size會越來越小
+				j =rnd.nextInt(cards.size()-nUsed);//範圍逐漸縮小
 						 					 //第一次進入while時，j將等於由0~51(表共52張牌存於cards)
 				//提示：隨機取一張牌牌，放到新的位置
 				//設usedCard, nUsed
-				usedCard.add(cards.get(j));//隨機取一張牌放到usedCard
-				cards.remove(cards.get(j));//移除剛剛取出放到usedCard裡的牌(刪除後還有資料，後面的資料會自動向前補上)
-				//nUsed++;
+				//測試//usedCard.add(cards.get(j));//隨機取一張牌放到usedCard
+				//測試//cards.remove(cards.get(j));//移除剛剛取出放到usedCard裡的牌(刪除後還有資料，後面的資料會自動向前補上)
+				//測試//nUsed++;
+				cards.add(cards.get(j));//將要取出的牌放到cards的最後
+				cards.remove(cards.get(j));//將原本的(將被取出的牌)刪除，這樣cards裡的總牌數才能不變
+				nUsed++;//每洗一張牌就加一
 			}//執行完後，cards裡將沒有牌，洗好的牌將會在usedCard裡
-			cards.addAll(usedCard);//把usedCard裡所有的牌放進cards裡(此時cards原本是空的)
-			usedCard.removeAll(usedCard);//移除usedCard裡所有的牌(usedCard將會變空的)
+			//測試//cards.addAll(usedCard);//把usedCard裡所有的牌放進cards裡(此時cards原本是空的)
+			//測試//usedCard.removeAll(usedCard);//移除usedCard裡所有的牌(usedCard將會變空的)
+			nUsed=0;
 		//洗還牌後使用過的牌的數量nUsed(發出的牌的數量)，將會重新變為零
 	}
-	public Card getOneCard(){//發一張牌出來，回傳Card object
+	//增(3)
+	//public Card getOneCard (boolean isOpened)
+	//- 拿到一張牌，修改原有方法，加入isOpened參數，決定發出去的牌是開著還是蓋起來的，若是開著的牌，加入openCard。（5pt）
+	//END(3)
+	public Card getOneCard(boolean isOpened){//發一張牌出來，回傳Card object,加入isOpened參數，決定發出去的牌是開著還是蓋起來的
 		//要檢查沒牌了 (牌都發完了) 怎麼辦？ shuffle() ！
 		//– 發出去的牌紀錄在ArrayList<Card> usedCard
 		//– 發了幾張牌？紀錄在private int nUsed=0
-		//System.out.println(nUsed);
+		//測試//System.out.println(nUsed);
 		nUsed++;//把發出去的牌數的牌的數量記錄在nUsed
+		usedCard=new ArrayList<Card>();
 		Card card=new Card(cards.get(nUsed-1).getSuit(),cards.get(nUsed-1).getRank());//取一張牌，此張牌將被使用(被發出)
-		usedCard.add(card);
+		usedCard.add(card);//將要被發出地牌紀錄在usedCard;
+		if(isOpened=true)//如果要發的是明牌，isOpened=true
+		{
+			openCard.add(card);//將得到的牌存進明牌中
+		}
 		//把發出去的牌數的牌的數量記錄在nUsed
 		//cards.remove(cards.get(0));//刪除發出去的牌
 		//card.printCard();
@@ -70,7 +93,13 @@ public class Deck {
 		}
 		return card;
 	}
-	//
+	//增(4)
+	public ArrayList getOpenedCard()//得到打開過的牌
+	{	
+		return openCard;//回傳此副牌中所有打開過的牌
+	}
+	//回傳此副牌中所有打開過的牌，意即openCard（5pt）
+	//END(4)
 	//TODO: Please implement the method to print all cards on screen (10 points)
 	/*public  void printDeck(){
 		//Hint: print all items in ArrayList<Card> cards, 
